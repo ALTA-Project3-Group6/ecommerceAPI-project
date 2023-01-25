9fc0be3c8fb23b92f317c0b5f70ad57ae6a2d88e
Muhamad Fauzan Putra <business.mfauzanptra@gmail.com>
2023-01-25 12:28:23 +0700

    chore: add migrate for order

--- migration/migration.go
+++ migration/migration.go
@@ -1,6 +1,7 @@
 package migration
 
 import (
+	order "ecommerceapi/features/order/data"
 	product "ecommerceapi/features/product/data"
 	user "ecommerceapi/features/user/data"
 
@@ -10,4 +11,5 @@ import (
 func Migrate(db *gorm.DB) {
 	db.AutoMigrate(user.User{})
 	db.AutoMigrate(product.Product{})
+	db.AutoMigrate(order.Order{})
 }
