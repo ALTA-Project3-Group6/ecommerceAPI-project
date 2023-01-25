faedb55573f37e6bc22244e2af3c42604404d4f5
icxz1 <alfianadityadwi@gmail.com>
2023-01-26 00:15:45 +0700

    feat: build handler response

--- features/cart/handler/handler.go
+++ features/cart/handler/handler.go
@@ -1,1 +1,38 @@
 package handler
+
+import (
+	"ecommerceapi/features/cart"
+
+	"github.com/labstack/echo/v4"
+)
+
+type cartControl struct {
+	srv cart.CartService
+}
+
+func New(srv cart.CartService) cart.CartHandler {
+	return &cartControl{
+		srv: srv,
+	}
+}
+
+func (cc *cartControl) AddCart() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		
+	}
+}
+func (cc *cartControl) ShowCart() echo.HandlerFunc {
+	return func(c echo.Context) error {
+
+	}
+}
+func (cc *cartControl) UpdateCart() echo.HandlerFunc {
+	return func(c echo.Context) error {
+
+	}
+}
+func (cc *cartControl) DeleteCart() echo.HandlerFunc {
+	return func(c echo.Context) error {
+
+	}
+}
