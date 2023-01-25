6f118e85eb658c0a999f83b3d610d1e004495e87
Muhamad Fauzan Putra <business.mfauzanptra@gmail.com>
2023-01-25 09:02:29 +0700

    fix: upload product photo after create query

--- features/product/data/query.go
+++ features/product/data/query.go
@@ -24,32 +24,31 @@ func (pq *productQuery) Add(userId uint, newProduct product.Core, productImage *
 	cnvP := CoreToData(newProduct)
 	cnvP.UserId = userId
 
-	if productImage != nil {
-		path, err := helper.UploadProductPhotoS3(*productImage, int(cnvP.ID))
-		if err != nil {
-			log.Println("\terror upload product photo: ", err.Error())
-			return product.Core{}, err
-		}
-		cnvP.ProductImage = path
-	}
-
 	err := pq.db.Create(&cnvP).Error
 	if err != nil {
 		log.Println("\tadd product query error: ", err.Error())
 		return product.Core{}, errors.New("server problem")
 	}
 
-	newProduct.ID = cnvP.ID
-	newProduct.UserId = cnvP.UserId
+	if productImage != nil {
+		path, err := helper.UploadProductPhotoS3(*productImage, int(cnvP.ID))
+		if err != nil {
+			log.Println("\terror upload product photo: ", err.Error())
+			return product.Core{}, err
+		}
+		qry := pq.db.First(&cnvP)
+		cnvP.ProductImage = path
+		qry.Save(&cnvP)
+	}
 
-	return newProduct, nil
+	return DataToCore(cnvP), nil
 }
 func (pq *productQuery) Update(userId, productId uint, updProduct product.Core, productImage *multipart.FileHeader) (product.Core, error) {
 	cnvP := CoreToData(updProduct)
 	cnvP.UserId = userId
 
 	if productImage != nil {
-		path, err := helper.UploadProductPhotoS3(*productImage, int(cnvP.ID))
+		path, err := helper.UploadProductPhotoS3(*productImage, int(productId))
 		if err != nil {
 			log.Println("\terror upload product photo: ", err.Error())
 			return product.Core{}, err
@@ -87,7 +86,7 @@ func (pq *productQuery) Delete(userId, productId uint) error {
 }
 func (pq *productQuery) GetAllProducts() ([]product.Core, error) {
 	allprod := []product.Core{}
-	err := pq.db.Raw("SELECT p.id, p.user_id, u.name username, p.name, product_image, description, stock, price FROM products p JOIN users u ON u.id = p.id WHERE p.deleted_at IS NULL").Scan(&allprod).Error
+	err := pq.db.Raw("SELECT p.id, p.user_id, u.name user_name, p.name, product_image, description, stock, price FROM products p JOIN users u ON u.id = p.id WHERE p.deleted_at IS NULL").Scan(&allprod).Error
 	if err != nil {
 		log.Println("\terror query get all product: ", err.Error())
 		return []product.Core{}, err
@@ -98,9 +97,9 @@ func (pq *productQuery) GetAllProducts() ([]product.Core, error) {
 func (pq *productQuery) GetUserProducts(userId uint) ([]product.Core, error) {
 	return []product.Core{}, nil
 }
-func (pq *productQuery) GetProductById(userId, productId uint) (product.Core, error) {
+func (pq *productQuery) GetProductById(productId uint) (product.Core, error) {
 	prod := product.Core{}
-	err := pq.db.Raw("SELECT p.id, p.user_id, u.name username, p.name, product_image, description, stock, price FROM products p JOIN users u ON u.id = p.id WHERE p.deleted_at IS NULL AND p.id = ?", productId).Scan(&prod).Error
+	err := pq.db.Raw("SELECT p.id, p.user_id, u.name user_name, p.name, product_image, description, stock, price FROM products p JOIN users u ON u.id = p.id WHERE p.deleted_at IS NULL AND p.id = ?", productId).Scan(&prod).Error
 	if err != nil {
 		log.Println("\terror query get all product: ", err.Error())
 		return product.Core{}, err
