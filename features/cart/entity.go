32b473a3bea40f81479656563845691d863a46b0
icxz1 <alfianadityadwi@gmail.com>
2023-01-25 14:38:40 +0700

    feat: build cart core struct and interface

--- features/cart/entity.go
+++ features/cart/entity.go
@@ -1,1 +1,32 @@
 package cart
+
+import "github.com/labstack/echo/v4"
+
+type Core struct {
+	ID        uint
+	UserID    uint
+	ProductID uint
+	Price     float64
+	Quantity  int
+}
+
+type CartHandler interface {
+	AddCart() echo.HandlerFunc
+	ShowCart() echo.HandlerFunc
+	UpdateCart() echo.HandlerFunc
+	DeleteCart() echo.HandlerFunc
+}
+
+type CartService interface {
+	AddCart(token interface{}, productId uint, newCart Core) (Core, error)
+	ShowCart(token interface{}) ([]Core, error)
+	UpdateCart(token interface{}, cartId uint, updCart Core) (Core, error)
+	DeleteCart(token interface{}, cartId uint) error
+}
+
+type CartData interface {
+	AddCart(userId uint, productId uint, newCart Core) (Core, error)
+	ShowCart(userId uint) ([]Core, error)
+	UpdateCart(userId uint, productId uint, updCart Core) (Core, error)
+	DeleteCart(userId uint, cartId uint) error
+}
