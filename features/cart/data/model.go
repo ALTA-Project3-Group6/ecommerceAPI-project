95059508e0272bd1d19f7401195a485104789d42
Muhamad Fauzan Putra <business.mfauzanptra@gmail.com>
2023-01-26 07:20:41 +0700

    refactor: cart struct

--- features/cart/data/model.go
+++ features/cart/data/model.go
@@ -4,18 +4,16 @@ import (
 	"ecommerceapi/features/cart"
 	product "ecommerceapi/features/product/data"
 	user "ecommerceapi/features/user/data"
-
-	"gorm.io/gorm"
 )
 
 type Cart struct {
-	gorm.Model
+	ID        uint
 	UserID    uint
 	ProductID uint
 	Quantity  int
 	Price     float64
-	Seller    user.User       `gorm:"foreignkey:UserId;association_foreignkey:ID"`
-	Product   product.Product `gorm:"foreignkey:ProductId;association_foreignkey:ID"`
+	Seller    user.User       `gorm:"foreignkey:UserID;association_foreignkey:ID"`
+	Product   product.Product `gorm:"foreignkey:ProductID;association_foreignkey:ID"`
 }
 
 func DataToCore(data Cart) cart.Core {
@@ -30,7 +28,7 @@ func DataToCore(data Cart) cart.Core {
 
 func CoreToData(data cart.Core) Cart {
 	return Cart{
-		Model:     gorm.Model{ID: data.ID},
+		ID:        data.ID,
 		UserID:    data.UserID,
 		ProductID: data.ProductID,
 		Quantity:  data.Quantity,
